373c7dea8705519d88a7a0ce6a4e94aa694d4cde
PhanAnh1811 <tuananh.61779@gmail.com>
2020-12-14 11:15:31 +0700

    update

--- MyFourReactNativeApp/App.js
+++ MyFourReactNativeApp/App.js
@@ -1,12 +1,26 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useReducer, useState } from 'react';
 import { Text, View, FlatList } from 'react-native';
 import ButtonCustom from './src/components/ButtonCustom';
 
+import reducer from './src/reducers/ColorReducer';
+
 const App = () => {
 
-  const [counter,setCounter]=useState(0);
+  const [counter, setCounter] = useState(0);
 
-  const COLOR_VALUE= 20;
+  const initialState = {
+    red: 0,
+    green: 0,
+    blue: 0
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { red, green, blue } = state;
+  console.log(state)
+
+
+  const COLOR_VALUE = 20;
   //Build app add color random
   const RandomRgb = () => {
     const red = Math.floor(Math.random() * 256);
@@ -14,15 +28,12 @@ const App = () => {
     const blue = Math.floor(Math.random() * 256);
     return `rgb(${red},${green},${blue})`;
   }
-  const [red, setRed] = useState(0);
-  const [blue, setBlue] = useState(0);
-  const [green, setGreen] = useState(0);
 
   const [colors, setColors] = useState([]);
 
   return (
     <View>
-      
+
       {/* <ButtonCustom name="Increase" func={()=>setCounter(counter+1)}/>
       <ButtonCustom name="Decrease" func={()=>setCounter(counter-1)}/>
       <Text>Current count:{counter}</Text>
@@ -39,19 +50,19 @@ const App = () => {
           }}
         />
       </View> */}
-      
+
       <View>
         <Text>Red</Text>
-        <ButtonCustom name="Increase Red" func={()=>setRed(red + COLOR_VALUE)}/>
-        <ButtonCustom name="Decrease Red" func={()=>setRed(red-COLOR_VALUE)}/>
+        <ButtonCustom name="Increase Red" func={() => dispatch({ type: 'red', payload: COLOR_VALUE })} />
+        <ButtonCustom name="Decrease Red" func={() => dispatch({ type: 'red', payload: COLOR_VALUE * -1 })} />
         <Text>Blue</Text>
-        <ButtonCustom name="Increase Blue" func={()=>setBlue(blue+COLOR_VALUE)}/>
-        <ButtonCustom name="Decrease Blue" func={()=>setBlue(blue-COLOR_VALUE)}/>
+        <ButtonCustom name="Increase Blue" func={() => dispatch({ type: 'blue', payload: COLOR_VALUE })} />
+        <ButtonCustom name="Decrease Blue" func={() => dispatch({ type: 'blue', payload: COLOR_VALUE * -1 })} />
         <Text>Green</Text>
-        <ButtonCustom name="Increase Green" func={()=>setGreen(green+COLOR_VALUE)}/>
-        <ButtonCustom name="Decrease Green" func={()=>setGreen(green-COLOR_VALUE)}/>
+        <ButtonCustom name="Increase Green" func={() => dispatch({ type: 'green', payload: COLOR_VALUE })} />
+        <ButtonCustom name="Decrease Green" func={() => dispatch({ type: 'green', payload: COLOR_VALUE * -1 })} />
       </View>
-      <View style={{width:100,height:100,backgroundColor:`rgb(${red},${green},${blue})`}}/>
+      <View style={{ width: 100, height: 100, backgroundColor: `rgb(${red},${green},${blue})` }} />
 
     </View>
   )
---
+++ MyFourReactNativeApp/src/reducers/ColorReducer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'red':
+            if (state.red + action.payload >= 0 && state.red + action.payload <= 255)
+                return { ...state, red: state.red + action.payload };
+            else
+                return state;
+        case 'blue':
+            if (state.blue + action.payload >= 0 && state.blue + action.payload <= 255)
+                return { ...state, blue: state.blue + action.payload };
+            else
+                return state;
+        case 'green':
+            if (state.green + action.payload >= 0 && state.green + action.payload <= 255)
+                return { ...state, green: state.green + action.payload };
+            else
+                return state;
+        default:
+            return state;
+    }
+}
+
+export default reducer; 
