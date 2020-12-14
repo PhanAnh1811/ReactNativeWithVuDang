303a0475523a82e34e67804002517f13aa26a125
PhanAnh1811 <tuananh.61779@gmail.com>
2020-12-14 10:15:36 +0700

    update

--- MyFourReactNativeApp/App.js
+++ MyFourReactNativeApp/App.js
@@ -2,9 +2,11 @@ import React, { Component, useState } from 'react';
 import { Text, View, FlatList } from 'react-native';
 import ButtonCustom from './src/components/ButtonCustom';
 
-
 const App = () => {
 
+  const [counter,setCounter]=useState(0);
+
+  const COLOR_VALUE= 20;
   //Build app add color random
   const RandomRgb = () => {
     const red = Math.floor(Math.random() * 256);
@@ -13,14 +15,18 @@ const App = () => {
     return `rgb(${red},${green},${blue})`;
   }
   const [red, setRed] = useState(0);
-  const [blue, setBlue] = useState(1);
-  const [green, setGreen] = useState(2);
+  const [blue, setBlue] = useState(0);
+  const [green, setGreen] = useState(0);
+
   const [colors, setColors] = useState([]);
-  console.log(colors);
 
   return (
     <View>
       
+      {/* <ButtonCustom name="Increase" func={()=>setCounter(counter+1)}/>
+      <ButtonCustom name="Decrease" func={()=>setCounter(counter-1)}/>
+      <Text>Current count:{counter}</Text>
+
       <ButtonCustom name="ADD A COLOR" func={() => setColors([...colors, RandomRgb()])} />
       <View>
         <FlatList
@@ -32,8 +38,21 @@ const App = () => {
             )
           }}
         />
+      </View> */}
+      
+      <View>
+        <Text>Red</Text>
+        <ButtonCustom name="Increase Red" func={()=>setRed(red + COLOR_VALUE)}/>
+        <ButtonCustom name="Decrease Red" func={()=>setRed(red-COLOR_VALUE)}/>
+        <Text>Blue</Text>
+        <ButtonCustom name="Increase Blue" func={()=>setBlue(blue+COLOR_VALUE)}/>
+        <ButtonCustom name="Decrease Blue" func={()=>setBlue(blue-COLOR_VALUE)}/>
+        <Text>Green</Text>
+        <ButtonCustom name="Increase Green" func={()=>setGreen(green+COLOR_VALUE)}/>
+        <ButtonCustom name="Decrease Green" func={()=>setGreen(green-COLOR_VALUE)}/>
       </View>
-     
+      <View style={{width:100,height:100,backgroundColor:`rgb(${red},${green},${blue})`}}/>
+
     </View>
   )
 }
